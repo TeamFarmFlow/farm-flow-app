993cb6b7e9ebd0afe58ab4e0cc0c979df4eee72e
최원영 <choewy32@gmail.com>
2026-04-01 16:40:56 +0900

    chore: use version

--- src/feature/version/hooks/use-version.tsx
+++ src/feature/version/hooks/use-version.tsx
@@ -1,52 +1,32 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect } from 'react';
 
 import { versionApi } from '../api';
 
 import { Toast } from '@app/shared/toast';
 
 export function useVersion() {
-  const [version, setVersion] = useState<string>(NPM_PACKAGE_VERSION);
-  const isReloadingRef = useRef(false);
-
   useEffect(() => {
-    let isMounted = true;
+    let reloading = false;
 
     const checkVersion = async () => {
-      const data = await versionApi.get();
+      const res = await versionApi.get();
+      reloading = res.version !== NPM_PACKAGE_VERSION;
+    };
 
-      if (!isMounted) {
-        return;
+    while (true) {
+      if (reloading) {
+        break;
       }
 
-      setVersion(data.version);
-    };
-
-    void checkVersion();
-
-    const intervalId = window.setInterval(() => {
       void checkVersion();
-    }, 10_000);
-
-    return () => {
-      isMounted = false;
-      window.clearInterval(intervalId);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!version || version === NPM_PACKAGE_VERSION || isReloadingRef.current) {
-      return;
     }
 
-    isReloadingRef.current = true;
     Toast.info('새 버전이 배포되었습니다. 페이지를 새로고침합니다.');
 
-    const timeoutId = window.setTimeout(() => {
-      window.location.reload();
-    }, 1_500);
+    const timeoutId = window.setTimeout(window.location.reload, 1_500);
 
     return () => {
       window.clearTimeout(timeoutId);
     };
-  }, [version]);
+  }, []);
 }
